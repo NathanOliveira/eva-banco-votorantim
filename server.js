372377b2cff1fccb61df440fcf5a45a5c988f958
Nathan Justino Oliveira <njustino@everis.com>
2020-11-09 12:40:40 -0300

    correcao port

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
   
 app = require("./config/server");
 
-// var port = process.env.PORT||3000;
-var port = 3030;
+var port = process.env.PORT||3000;
 
 app.listen(port, function () {
     console.log(`Server runing on ${port}`);
