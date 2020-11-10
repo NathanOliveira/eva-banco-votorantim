4b1deef119ac81e2014eb25c94c783ad46663fd5
Nathan Justino Oliveira <njustino@everis.com>
2020-11-09 21:53:43 -0300

    ServiceCell random

--- app/controller/ServiceCellController.js
+++ app/controller/ServiceCellController.js
@@ -1,12 +1,15 @@
-function ServiceCell(req,resp) {
-    var option = req.query.option;
+function ServiceCell(req, resp) {
+    const months = ["VALID", "NOT_VALID","VALID"];
+    const random = Math.floor(Math.random() * months.length);
+    
+    var option = months[random];
     var response = {
         openContext: req.body.openContext,
         visibleContext: req.body.visibleContext,
         hiddenContext: req.body.hiddenContext,
         option
     }
-    
+
     resp.status(200).send(response);
 }
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
   
 app = require("./config/server");
 
-var port = process.env.PORT||3000;
+var port = process.env.PORT||3030;
 
 app.listen(port, function () {
     console.log(`Server runing on ${port}`);
